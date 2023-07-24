ab694f8cfc981333cf22652fe260d7014c58f4cf
Byishimo Cedrick <byishimocedrick@gmail.com>
2023-07-24 13:14:45 +0200

    first commit

--- Exercise 1 - Word Highlighter/highlighter.js
+++ Exercise 1 - Word Highlighter/highlighter.js
@@ -3,6 +3,8 @@ let z = para.textContent.split(" ")
 
 function CheckDuplicate(z){
     z = z.filter((item,index) => z.indexOf(item) === index).join(" ")
+
+
     let y = para.textContent
     for(let x of z){
         if(y.matchAll(x)){

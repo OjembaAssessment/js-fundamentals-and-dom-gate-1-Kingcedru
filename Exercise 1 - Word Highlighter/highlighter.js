c07f45fc1e6eb065570fa8f9f65b662379af9386
Byishimo Cedrick <byishimocedrick@gmail.com>
2023-07-24 13:06:35 +0200

    first commit

--- Exercise 1 - Word Highlighter/highlighter.js
+++ Exercise 1 - Word Highlighter/highlighter.js
@@ -1,3 +1,15 @@
+let para = document.getElementById("myParagraph")
+let z = para.textContent.split(" ")
 
+function CheckDuplicate(z){
+    z = z.filter((item,index) => z.indexOf(item) === index).join(" ")
+    let y = para.textContent
+    for(let x of z){
+        if(y.matchAll(x)){
+            para.textContent = z
+            para.style.color = "blue"
+        }
+    }    
+}
 
-console.log("Exercise 1 - Word Highlighter");
+console.log(CheckDuplicate(z));
